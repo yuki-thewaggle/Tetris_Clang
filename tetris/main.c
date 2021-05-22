53625279c74f254464210b2197160bb05ea8c762
yuki-thewaggle <36552360+yuki-thewaggle@users.noreply.github.com>
2021-05-22 16:57:51 +0900

    配布用

--- tetris/main.c
+++ tetris/main.c
@@ -3,9 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
-//#include <conio.h>
-#include "conio.h"  //for mac
-#include <curses.h> //for mac
+#include <conio.h>
 
 #define FIELD_HEIGHT 21 //枠を含む
 #define FIELD_WIDTH 12  //枠を含む
